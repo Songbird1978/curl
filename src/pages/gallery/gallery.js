85cb0185c03bfc9ecde88e2db606254116de3825
Hayley Callender <118288349+Songbird1978@users.noreply.github.comgit>
2023-11-26 00:48:00 +0000

    setting up gallery buttons

--- src/pages/gallery/gallery.js
+++ src/pages/gallery/gallery.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './gallery.css';
 import Grid from '@mui/material/Grid';
+import { Link } from "react-router-dom";
 //import { motion } from "framer-motion";
 import '../../index.css';
-import LiveSlider from '../../components/simpleSlider/liveSlider';
-import { LiveImages } from '../../components/simpleSlider/imageData.js';
+//import LiveSlider from '../../components/simpleSlider/liveSlider';
+//import { LiveImages } from '../../components/simpleSlider/imageData.js';
+import frenchTV from '../../assets/liveImages/French TV.jpg';
+import liveGallery from '../gallery/liveGallery.js';
 
 
 function Gallery() {
@@ -15,8 +18,36 @@ function Gallery() {
 
             <Grid container className="galleryGrid" sx={{ backgroundColor: 'transparent', display: 'flex' }}>
 
-                <Grid item className="galleryItem" xs={12} sx={{}}>
-                    <LiveSlider className='LiveSlider' LiveImages={LiveImages} sliderHeight="500px" />
+                <Grid item className="galleryItem" xs={12}  sm={6} md={6}>
+                
+                    <Link to={liveGallery} className="galleryLiveLink">
+                        <img src={frenchTV} className="buttonImage" alt="live at the Black Box">
+
+                        </img>
+                    </Link>
+                
+                </Grid>
+
+                <Grid item className="galleryItem" xs={12}  sm={6} md={6} >
+                <Link to={liveGallery} className="galleryLiveLink">
+                        <img src={frenchTV} className="buttonImage" alt="live at the Black Box">
+
+                        </img>
+                    </Link>
+                </Grid>
+                <Grid item className="galleryItem" xs={12}  sm={6} md={6} sx={{}}>
+                <Link to={liveGallery} className="galleryLiveLink">
+                        <img src={frenchTV} className="buttonImage" alt="live at the Black Box">
+
+                        </img>
+                    </Link>
+                </Grid>
+                <Grid item className="galleryItem" xs={12}  sm={6} md={6} sx={{}}>
+                <Link to={liveGallery} className="galleryLiveLink">
+                        <img src={frenchTV} className="buttonImage" alt="live at the Black Box">
+
+                        </img>
+                    </Link>
                 </Grid>
 
             </Grid >
