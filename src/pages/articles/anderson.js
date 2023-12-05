9d885189fbe17c48e921b3382e00233fb9c3fa30
Songbird1978 <118288349+Songbird1978@users.noreply.github.com>
2023-12-05 19:39:05 +0000

    added galleries and some other styling

--- src/pages/articles/anderson.js
+++ src/pages/articles/anderson.js
@@ -5,25 +5,29 @@ import '../../index.css';
 import { motion } from "framer-motion";
 import "../biography/biography.css";
 import '../../index.css';
+import data from './Data.json';
 
 function Anderson() {
+
+    const articleIdToTarget = 0;
+  const targetedArticle = data.find(article => article.id === articleIdToTarget);
+
+  if (!targetedArticle) {
+    // Handle the case where the article with the specified id is not found
+    return <p>Article not found</p>;
+  }
+
     return (
 
         <motion.div className="containerConstruction" initial={{ width: 0 }} animate={{ width: "100%" }} exit={{ x: window.innerWidth, transition: { duration: 0.1 } }}>
 
             <div className="mxnifesto" >
-                <h1 className="title">Anderson Shelter</h1>
-                <p className="manifestoParagraph">{
-
-                    `
-It has been seven years since the release of the third album from CURL 'Shapeshifters' but the question you have to ask is, 'Who is Anderson?'
-
-The worst superhero we have ever met. Collaborating with this guy, has slowed development for the creation of the forthcoming, 2024 album, 'Interferometry'. 
-
-In 1940, the British Government asked the people to build Anderson Shelters in their gardens as protection from the Blitz. "A feelgood factor", "Do your bit", yet, completely useless, just like our superhero associate. The new CURL single 'Anderson Shelter' will help you understand how CURL have been recovering from their self-inflicted bombing raids.
-
-`}
-                </p>
+               
+                <h1 key={targetedArticle.id} className="title">{targetedArticle.title}</h1>
+                <h3>{targetedArticle.description}</h3>
+                <img src={targetedArticle.image} alt={targetedArticle.imageName}></img>
+                <p className="manifestoParagraph">{targetedArticle.info}</p>
+                <h5>{targetedArticle.copyright}</h5>
             </div>
 
             <Outlet />
